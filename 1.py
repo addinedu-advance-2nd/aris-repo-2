f27644c599a4c9aa3eb3856236959a825d878c3a
HWAN-CHUL-LA <fkghkscjf@gmail.com>
2024-10-31 14:14:05 +0900

    upload

---
+++ 1.py
@@ -0,0 +1,20 @@
+from STT.stt import my_stt
+from RAG.ragpipeline import RagPipeline
+
+import sounddevice as sd
+
+rag = RagPipeline()
+
+
+while True :
+    my_speech = my_stt()
+    if my_speech == "종료" :
+        break
+    else :
+        print('대답')
+        print(my_speech)
+        answer = rag.generate_answer(my_speech)
+
+        print(answer)
+
+
